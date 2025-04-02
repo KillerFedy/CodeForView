8486d138d1cec2f59ab7c44b871c8f2b8c931539
Фёдор Зуев <zuev.f.s@gmail.com>
2025-04-02 21:51:31 +1000

    Init

---
+++ TransactionService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeForView
+{
+    public class TransactionService
+    {
+        private async Task<string> TransactionPay(ProviderTransactionRequest providerTransactionRequest)
+        {
+            var errorMsg = "";
+            var transactionInfo = await transactionInfoRepository.GetItemByQueryAsync(x =>
+            !x.IsErrorResponse &&
+            !x.IsPaymentEnd &&
+            x.IsTransactionAuth
+            && x.Rrn == providerTransactionRequest.Rrn && x.TerminalId == providerTransactionRequest.Terminal,
+            include: x => x.Include(x => x.PaymentProviderInfo));
+            if (transactionInfo == null)
+            {
+                errorMsg = "Не удалось получить объект транзакции авторизации платежа";
+                logger.LogError(errorMsg);
+                return ErrorInitializer.InitializeErrorToString(providerTransactionRequest.Stan, errorMsg);
+            }
+            providerTransactionRequest.SetPcode(transactionInfo.PaymentProviderInfo.ProviderCodeName != null ?
+                transactionInfo.PaymentProviderInfo.ProviderCodeName : providerTransactionRequest.Pcode);
+            var providerTransactionResponse = await paymentProviderGateway.ProvideTransactionRequest(transactionInfo.PaymentProviderInfo.ProviderUrl,
+                providerTransactionRequest);
+            if (providerTransactionResponse == null)
+            {
+
+                errorMsg = $"Банковский сервер {transactionInfo.PaymentProviderInfo.ProviderName} " +
+                $"не вернул ответа";
+                logger.LogError(errorMsg);
+                return ErrorInitializer.InitializeErrorToString(providerTransactionRequest.Stan, errorMsg);
+            }
+            var isErrorResponse = providerTransactionResponse.MBilling.Error != null;
+            if (!isErrorResponse) InitProviderTransactionResponseInfoByPcode(providerTransactionRequest.Pcode,
+                providerTransactionResponse.MBilling.Payment);
+            var response = ObjectSerializer.SerializeToXml(providerTransactionResponse);
+            using (var transaction = await transactionInfoRepository.BeginTransactionAsync())
+            {
+                try
+                {
+                    var transactionPayInfo = new TransactionInfo(providerTransactionRequest.Rrn, providerTransactionRequest.Phone,
+                        providerTransactionRequest.Amount, providerTransactionRequest.Terminal, providerTransactionRequest.Currency);
+                    transactionPayInfo.InitTransactionAfterProviderRequest(isErrorResponse, providerTransactionRequest.Pcode, response);
+                    transactionPayInfo.InitPaymentProviderInfoId(transactionInfo.PaymentProviderInfoId);
+                    transactionInfo.ProcessPaymentEnd();
+                    transactionPayInfo.ProcessPaymentEnd();
+                    await transactionInfoRepository.UpdateAsync(transactionInfo);
+                    await transactionInfoRepository.InsertAsync(transactionPayInfo);
+                    await transaction.CommitAsync();
+                }
+                catch (Exception ex)
+                {
+                    await transaction.RollbackAsync();
+                    logger.LogError(ex, "Ошибка при подготовке данных к обновлению/обновлении БД во время выполнения bank_payment");
+                }
+            }
+            return response;
+        }
+    }
+}
---
+++ UnitTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeForView
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void CreateUser_ShouldOk()
+        {
+
+            _userRepository.Setup(repository => repository.GetByLogin(It.IsAny<string>()))
+                        .Returns(() => null);
+
+            var res = _userService.CreateUser(new User(default, "AnotherLogin", "TestUser", "88005553535", "Sharp", default));
+
+            Assert.True(res.Success);
+            Assert.Equal(string.Empty, res.Error);
+        }
+
+
+
+        [Fact]
+        public void EditSchedule_ShouldOk()
+        {
+            Schedule schedule = new Schedule(default, new DateTime(1, 1, 1), new DateTime(1, 1, 1));
+
+            _scheduleServiceMock.Setup(repository => repository.EditSchedule(schedule))
+                .Returns(() => new Schedule(default, new DateTime(1, 1, 1), new DateTime(1, 1, 1)));
+
+            var res = _scheduleService.EditSchedule(schedule);
+
+            Assert.True(res.Success);
+            Assert.Equal(string.Empty, res.Error);
+        }
+    }
+}
